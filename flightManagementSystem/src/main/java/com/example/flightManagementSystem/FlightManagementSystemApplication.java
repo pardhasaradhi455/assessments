36f7834eeee98ec11bddbadb17c2a64179e08296
pardhasaradhi455 <pardhasaradhimalluru@gmail.com>
2023-04-20 17:08:35 +0530

    new project added

---
+++ flightManagementSystem/src/main/java/com/example/flightManagementSystem/FlightManagementSystemApplication.java
@@ -0,0 +1,14 @@
+package com.example.flightManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightManagementSystemApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
